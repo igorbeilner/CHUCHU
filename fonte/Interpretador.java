da716ec4bde6f77ae738bc90293133f3ae0035cf
igorbeilner <igor.beilner@hotmail.com>
2014-06-20 19:39:17 -0300

    começando funções, já guarda o endereço, tem como declarar, mas ainda não pode ser utilizada!

--- fonte/Interpretador.java
+++ fonte/Interpretador.java
@@ -18,6 +18,9 @@ class Interpretador{
 	private Alu solve;
 	private Scanner s;	
 	private int flagElse;
+	private int flagF;
+	private Funcao[] vetorFunc;
+	private int posicaoProg;
 	
 	public Interpretador(){
 		loop=new Pilha();
@@ -25,6 +28,9 @@ class Interpretador{
 		solve = new Alu();
 		s=new Scanner(System.in);
 		flagElse = 0;
+		flagF = 0;
+		vetorFunc = new Funcao[1000];
+		posicaoProg = 0;
 	}
 
 	private void imprime(String expressao){
@@ -127,6 +133,29 @@ class Interpretador{
 		return v;
 	}
 
+	private int existeFuncao(String nome) {
+		nome = nome.trim();
+		int i;
+		for(i=0; i<posicaoProg; i++) {
+			if(vetorFunc[i].getNomeFuncao().equals(nome)) {
+				return vetorFunc[i].getIndice();
+			}
+		}
+		return 2005;
+	}
+
+	private void criaFuncao(String nome, int i) {
+		nome = nome.trim();
+		if(existeFuncao(nome) == 2005) {
+			flagF = 1;
+			vetorFunc[posicaoProg] = new Funcao(nome, i);
+			posicaoProg++;
+		} else {
+			System.out.println("Nome de funcoes replicadas");
+			System.exit(0);
+		}
+	}
+
 	public boolean interpreta(String[] cmd){
 		int i;
 		char instrucao;
@@ -146,6 +175,8 @@ class Interpretador{
 			cmd[i]=cmd[i].replace("get",".");
 			cmd[i]=cmd[i].replace("else","^");
 			cmd[i]=cmd[i].replace("vet","_");
+			cmd[i]=cmd[i].replace("end funcao","´");
+			cmd[i]=cmd[i].replace("funcao","~");
 		}
 
 		for(i=0; i<cmd.length && cmd[i] != null; i++){
@@ -153,145 +184,159 @@ class Interpretador{
 			String a = cmd[i].replace(" ","");
 			a = cmd[i].replace("	","");
 			if(a.equals("") == false && b[0].equals("") == false){				
+				
+				if(cmd[i].contains("´")) {
+					flagF = 0;
+					i++;
+				}
+
 				//Retira o token e salva na instrucao
 				instrucao=cmd[i].charAt(0);
 				temp=cmd[i].substring(1);
 				temp=temp.trim();
-
-				switch(instrucao){
-					case'#':
-						//atualiza variavel
-						String[] newvar=temp.split("=");
-						double n = (newvar.length > 1)? solve.leExpressao(newvar[1], mem): 0;
-						mem.atualizaVar(n, newvar[0].trim());
-						break;
-					case '§': 
-						//imprime 
-						this.imprime(temp);
-						break;
-					case '_': 
-						//vetores
-						String[] k = cmd[i].split("\\+|\\*|\\-|\\%|\\/|\\[|\\]|=");
-						String[] op = cmd[i].split("\\[|\\]|=");
-						int tam = 0;
-						double v = 0.0;
-						double v2 = 0.0;
-						int indice = 0;
-
-						tam = verificaIndice(k[1]);
-
-						if (k.length == 4 || k.length == 5) {
-							if(mem.verificaVetor(k[3]) == 0) {
-								v = verificaatribuicao(k[3]);
-							}					
-						}
-
-						if (k.length == 5) {
-							if(mem.verificaVetor(k[3]) != 0) {
-								indice = indice = verificaIndice(k[4]);
-								v = mem.leVetor(k[3], indice);
-							} else {
-								v2 = verificaatribuicao(k[4]);
-								v = ula(op[3], v, v2);						
-							}
-						}
-
-						else if(k.length == 6) {
-							indice = verificaIndice(k[5]);
-							v = mem.leVetor(k[4], indice);
-							v2 = verificaatribuicao(k[3]);
-							v = ula(op[3], v2, v);
-						}
-
-						else if(k.length == 7) {
-							indice = verificaIndice(k[4]);
-							v = mem.leVetor(k[3], indice);
-							v2 = verificaatribuicao(k[6]);
-							v = ula(op[5], v, v2);
-						}
-
-						else if(k.length == 8) {
-							indice = verificaIndice(k[4]);
-							v = mem.leVetor(k[3], indice);
-							indice = verificaIndice(k[7]);
-							v2 = mem.leVetor(k[6], indice);
-							v = ula(op[5], v, v2);
-						}
-
-						int j;
-						k[2] = k[2].trim();
-						j = mem.verificaVetor(k[2]);
-						if(j == 0) {
-							mem.criaVetor(k[2], tam);
-							if(k.length == 4) {
-								System.out.println("tentando criar vetor que já existe");
-								System.exit(0);
-							}
-						} else {
-							if(k.length >= 4 && k.length <= 8) {
-								mem.atribuiVetor(k[2], tam, v);
-							} else {
-								System.out.println("metodo de atricuicao invalido");
-								System.exit(0);
-							}
-						}
-						break;
-					case '@': 
-						//if
-						if(solve.leIf(temp, mem)==false)
-							i=desvio(cmd, i, instrucao);
-						else flagElse = 1;
-						break;
-					case '^':
-						//else
-						if(flagElse == 1) {
-							i=desvio(cmd, i, instrucao);
-							flagElse = 0;
-						}
-						break;
-					case 'o':
-						//end else;
-						break;
-					case '!': 
-						//loop;
-						if(solve.leIf(temp, mem)==true)
-							loop.push(i);
-						else
-							i = this.desvio(cmd, i, instrucao);
-						break;
 				
-					case '¬':
-						//end loop;
-						if(loop.vazio())				
-							i=loop.topo()-1;
-						loop.pop();
-						break;
-		
-					case '€': 
-						//break;
-						i=desvio(cmd, i, '!');
-						loop.pop();
-						break;
-					case '.':
-						//scan
-						double x = s.nextDouble();
-						if(mem.variavelExiste(temp) == true) {
-							mem.atualizaVar(x, temp);
-						} else {
-							String u[] = temp.split("\\[|\\]");
-							int qw;
-							if(mem.verificaVetor(u[0]) != 0) {
-								qw = verificaIndice(u[1]);
-								mem.atribuiVetor(u[0], qw, x);
+				if(flagF == 0) {
+					switch(instrucao){
+						case'#':
+							//atualiza variavel
+							String[] newvar=temp.split("=");
+							double n = (newvar.length > 1)? solve.leExpressao(newvar[1], mem): 0;
+							mem.atualizaVar(n, newvar[0].trim());
+							break;
+						case '§': 
+							//imprime 
+							this.imprime(temp);
+							break;
+						case '_': 
+							//vetores
+							String[] k = cmd[i].split("\\+|\\*|\\-|\\%|\\/|\\[|\\]|=");
+							String[] op = cmd[i].split("\\[|\\]|=");
+							int tam = 0;
+							double v = 0.0;
+							double v2 = 0.0;
+							int indice = 0;
+
+							tam = verificaIndice(k[1]);
+
+							if (k.length == 4 || k.length == 5) {
+								if(mem.verificaVetor(k[3]) == 0) {
+									v = verificaatribuicao(k[3]);
+								}					
 							}
-						}
-						break;
-					case '$':
-						//end if
-						break;
-					default: 
-						System.out.println(" syntax error "+ (i+1));
-						return false;
+
+							if (k.length == 5) {
+								if(mem.verificaVetor(k[3]) != 0) {
+									indice = indice = verificaIndice(k[4]);
+									v = mem.leVetor(k[3], indice);
+								} else {
+									v2 = verificaatribuicao(k[4]);
+									v = ula(op[3], v, v2);						
+								}
+							}
+
+							else if(k.length == 6) {
+								indice = verificaIndice(k[5]);
+								v = mem.leVetor(k[4], indice);
+								v2 = verificaatribuicao(k[3]);
+								v = ula(op[3], v2, v);
+							}
+
+							else if(k.length == 7) {
+								indice = verificaIndice(k[4]);
+								v = mem.leVetor(k[3], indice);
+								v2 = verificaatribuicao(k[6]);
+								v = ula(op[5], v, v2);
+							}
+
+							else if(k.length == 8) {
+								indice = verificaIndice(k[4]);
+								v = mem.leVetor(k[3], indice);
+								indice = verificaIndice(k[7]);
+								v2 = mem.leVetor(k[6], indice);
+								v = ula(op[5], v, v2);
+							}
+
+							int j;
+							k[2] = k[2].trim();
+							j = mem.verificaVetor(k[2]);
+							if(j == 0) {
+								mem.criaVetor(k[2], tam);
+								if(k.length == 4) {
+									System.out.println("tentando criar vetor que já existe");
+									System.exit(0);
+								}
+							} else {
+								if(k.length >= 4 && k.length <= 8) {
+									mem.atribuiVetor(k[2], tam, v);
+								} else {
+									System.out.println("metodo de atricuicao invalido");
+									System.exit(0);
+								}
+							}
+							break;
+						case '@': 
+							//if
+							if(solve.leIf(temp, mem)==false)
+								i=desvio(cmd, i, instrucao);
+							else flagElse = 1;
+							break;
+						case '^':
+							//else
+							if(flagElse == 1) {
+								i=desvio(cmd, i, instrucao);
+								flagElse = 0;
+							}
+							break;
+						case 'o':
+							//end else;
+							break;
+						case '!': 
+							//loop;
+							if(solve.leIf(temp, mem)==true)
+								loop.push(i);
+							else
+								i = this.desvio(cmd, i, instrucao);
+							break;
+					
+						case '¬':
+							//end loop;
+							if(loop.vazio())				
+								i=loop.topo()-1;
+							loop.pop();
+							break;
+			
+						case '€': 
+							//break;
+							i=desvio(cmd, i, '!');
+							loop.pop();
+							break;
+						case '~': 
+							//funcao
+							System.out.println(existeFuncao(temp));
+							System.out.println(vetorFunc.length);
+							criaFuncao(temp, i);
+							break;
+						case '.':
+							//scan
+							double x = s.nextDouble();
+							if(mem.variavelExiste(temp) == true) {
+								mem.atualizaVar(x, temp);
+							} else {
+								String u[] = temp.split("\\[|\\]");
+								int qw;
+								if(mem.verificaVetor(u[0]) != 0) {
+									qw = verificaIndice(u[1]);
+									mem.atribuiVetor(u[0], qw, x);
+								}
+							}
+							break;
+						case '$':
+							//end if
+							break;
+						default: 
+							System.out.println(" syntax error "+ (i+1));
+							return false;
+					}
 				}
 			}
 		}	
